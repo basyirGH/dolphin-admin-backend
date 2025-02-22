2e1fbe0591b1cae60fe3476d3099e0c5b06ad3f9
Basyir <basyirzainuddin@gmail.com>
2025-02-22 18:52:41 +0800

    removed actuator

--- src/main/java/com/dolphin/adminbackend/controller/HomeController.java
+++ src/main/java/com/dolphin/adminbackend/controller/HomeController.java
@@ -2,21 +2,22 @@ package com.dolphin.adminbackend.controller;
 
 import java.util.HashMap;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/api/v1/home")
+@RequestMapping("/")
 public class HomeController {
 
     @ResponseBody
     @RequestMapping(value = "",method = RequestMethod.GET)
-    public HashMap<String, String> hello(){
-        HashMap<String, String> response = new HashMap<String, String>();
-        response.put("code", "TOKEN_IS_VALID");
-        return response;
+    public ResponseEntity<?> hello(){
+        return ResponseEntity.status(HttpStatus.OK).body("Welcome to Dolphin!"); // HTTP 200
     }
 
 }
